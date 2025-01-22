8bc6757e3ba8ba59776b058acec6834b2711acb8
Алексей <mehabiw873@fundapk.com>
2025-01-22 12:14:26 +0200

    Поправлено регулярное выражение.

--- index.php
+++ index.php
@@ -5,11 +5,7 @@
 		$pureNumber = preg_replace(["/[^\d]/", "/^00/"], "", $number);
 		if (
 			(strlen($pureNumber) < 10) ||
-			!(bool)preg_match(
-				'/^((\((\+|00(\s|-)?)?\d+\))|((\+|00(\s|-)?)?\d+))(\s|-)?' .
-				'(\(\d+\)(\s|-)?)?(\d+(\s|-)?)+\d+$/',
-				$number
-			)
+			!(bool)preg_match('/^((\(\+?\d+\))|(\+?\d+))(\s|-)?(\(\d+\)(\s|-)?)?(\d+(\s|-)?)+\d+$/', $number)
 		)
 			return false;
 		else
