fce1f0d1d0903e730540a0dfd10c68b7272f73ce
Алексей <mehabiw873@fundapk.com>
2025-01-22 09:55:36 +0200

    Первая закачка файлов.

---
+++ form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Проверка номера телефона</title>
+	<style>
+		body { font: bold 16px sans-serif; }
+		input { font-size: 18px; }
+		form { display: inline-block; }
+		form input[type="text"] { font-family: monospace; }
+		#msgTitle { margin: 1em auto 0.5em; }
+		#message { margin-left: 1vw; color: #3A8; }
+		.errorMsg { color: #C00 !important; }
+	</style>
+</head>
+<body>
+	<div id="form_container">
+		<form id="main_form">
+			<input type=text name="phoneNumber" placeholder="Введите номер..." />
+		</form>
+		<input type=button id="checkNumberBtn" value="Проверить" />
+	</div>
+	<div id="msgTitle">Результат:</div>
+	<div id="message"></div>
+
+	<script>
+		function checkNumber() {
+			const messageContainer = document.getElementById("message");
+			messageContainer.innerHTML = "";
+
+			fetch("", {
+				method: "POST",
+				cache: "no-cache",
+				body: new FormData(document.getElementById("main_form"))
+			}).then(ans => ans.json()).then(res => {
+				messageContainer.classList[res.successful ? "remove" : "add"]("errorMsg");
+				messageContainer.innerText = res.successful ?
+					`Страна/территория: ${res.country}.` :
+					"Неправильный номер.";
+			}).catch(() => alert("Ошибка."));
+		}
+
+		document.getElementById("checkNumberBtn").addEventListener("click", checkNumber);
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+	require 'config.php';
+
+	function checkAndClearPhoneNumber($number) {
+		$pureNumber = preg_replace(["/[^\d]/", "/^00/"], "", $number);
+		if (
+			(strlen($pureNumber) < 10) ||
+			!(bool)preg_match('/^((\(\+?\d+\))|(\+?\d+))(\s|-)*(\(\d+\)(\s|-)*)?(\d+(\s|-)*)+\d+$/', $number)
+		)
+			return false;
+		else
+			return $pureNumber;
+	}
+
+	if (isset($_POST['phoneNumber'])) {
+		$number = checkAndClearPhoneNumber($_POST['phoneNumber']);
+		$successful = (bool)$number;
+		$country = null;
+
+		if ($successful) {
+			$data = [];
+
+			$db = new mysqli($db_server, $db_user, $db_password, $db_name);
+			$db->set_charset('utf8');
+
+			$ans = $db->query("
+				SELECT * FROM country_codes WHERE
+					LENGTH(subcodes) > 0 AND '$number' LIKE CONCAT(code, '%') AND
+					'$number' REGEXP CONCAT('^', code, '(', subcodes, ')')
+			");
+			$data = $ans->fetch_assoc();
+			$ans->free_result();
+
+			if (!$data) {
+				$ans = $db->query("
+					SELECT * FROM country_codes WHERE
+						LENGTH(subcodes) = 0 AND '$number' LIKE CONCAT(code, '%')
+				");
+				$data = $ans->fetch_assoc();
+				$ans->free_result();
+			}
+
+			if (!$data) $successful = false;
+			else $country = $data['name'];
+		}
+
+		echo JSON_encode([
+			'successful' => $successful,
+			'country' => $country,
+		]);
+	}
+	else require 'form.php';
+?>
